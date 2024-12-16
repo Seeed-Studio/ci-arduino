4467f4cc39f288a955804f292fd1b7e3a0a03332
Lesords <2385342343@qq.com>
2024-12-16 09:40:09 +0000

    feat: add a script to handle the operations related to issue generation

---
+++ tools/issue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+resultFile=compile.failed
+issueFile=issue.log
+bodyText="Automatic compilation failed
+
+$(cat $issueFile)
+
+You can view the details through the following link
+
+$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID
+
+[$(date +"%Y-%m-%d %H:%M %Z %A")]"
+
+if [ -f $resultFile ]; then
+    echo "Compilation failed"
+    issueNumber=$(gh issue list --json number,title --jq 'first(.[] | select(.title == "ci: build failed")).number')
+
+    if [ "$issueNumber" ]; then
+        gh issue comment $issueNumber --body "$bodyText"
+    else
+        gh issue create --title "ci: build failed" --body "$bodyText"
+    fi
+
+    exit 1
+else
+    echo "Compilation successful"
+fi
